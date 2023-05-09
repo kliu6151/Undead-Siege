34a008ede21292cdf562ae19465790543788edbe
jcJoeyChan <joeychan1012@gmail.com>
2023-05-09 13:15:03 -0400

    Level 3 Map and Music

---
+++ dist/assets/tilemaps/imageslvl3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="imageslvl3" tilewidth="16" tileheight="16" tilecount="800" columns="40">
+ <image source="imageslvl3.png" width="640" height="320"/>
+</tileset>
